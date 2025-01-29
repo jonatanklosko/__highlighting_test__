1c8c96e24309f68a7ea66a6db33d2b9a2c67a1f8
Jonatan Kłosko <jonatanklosko@gmail.com>
2025-01-29 23:49:33 +0700

    Update

--- test.ex
+++ test.ex
@@ -1,9 +1,12 @@
 assigns = %{x: 1}
 
+x = 1
+string = "interpolated-#{x + 1}-value"
+
 ~H"""
-<div class="flex" data-x={"interpolated-#{x}-value"}>
+<div class="flex" data-x={"interpolated-#{@x + 1}-value"} data-y={Integer.to_string(@x)}>
   <span>Value:</span>
-  <span><%= x + 1 %></span>
-  <span>{x + 1}</span>
+  <span><%= @x + 1 %></span>
+  <span>{@x + 1}</span>
 </div>
 """
